ddc182e4fed8a0c35dc0c962cc43269c711f3d2b
Katherine Ross <katylahv@gmail.com>
2019-11-20 16:16:19 -0600

    new images and fonts updated

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Fave PIX';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://diyuehb.com/images/daisy-outline-clip-art-3.png';
+  image3 = 'https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_240/public/TOY_Chihuahua-%28Smooth-Coat%29.jpg?itok=FnZXAZI6';
 
   constructor() { }
 

4f24bdbe0d60d233a918f5c5d1e5f8af93bed5f1
Katherine Ross <katylahv@gmail.com>
2019-11-20 18:28:29 -0600

    links added

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-title = "Katy's first Angular!"
+title = "Katy's first Angular!";
   constructor() { }
 
   ngOnInit() {
